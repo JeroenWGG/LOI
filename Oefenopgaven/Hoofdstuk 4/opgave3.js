4862e674d896f226d736e415adc683aa0bfede25
Jeroen Wiersma <J.Wiersma@plein.nl>
2020-09-04 14:28:43 +0200

    Linter

--- Oefenopgaven/Hoofdstuk 4/opgave3.js
+++ Oefenopgaven/Hoofdstuk 4/opgave3.js
@@ -6,36 +6,35 @@ let resultaat = document.querySelector("#result");
 //Kassa constructor
 function Kassa(saldo) {
   this.saldo = saldo;
-  this.telOp = function(bedrag) {
+  this.telOp = function (bedrag) {
     this.saldo = this.saldo + Number(bedrag);
   };
-  this.totaal = function(bedrag) {
-    return this.saldo
+  this.totaal = function (bedrag) {
+    return this.saldo;
   };
 }
 
 //Checken of enter wordt gedrukt en is ingevuld
-bedragVeld.addEventListener("keyup", function(event){
-    if (event.keyCode === 13) {
-        //default event cancellen
-        event.preventDefault();
-        if(checkForNumber(bedragVeld.value)){
-          newKassa.telOp(bedragVeld.value);
-          bedragVeld.value = "";
-        } else {
-          alert("Je moet een getal invullen...")
-        }
+bedragVeld.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    //default event cancellen
+    event.preventDefault();
+    if (checkForNumber(bedragVeld.value)) {
+      newKassa.telOp(bedragVeld.value);
+      bedragVeld.value = "";
+    } else {
+      alert("Je moet een getal invullen...");
     }
+  }
 });
 
 //Totaal berekenen en tonen op pagina
-totaalKnop.addEventListener("click", function(event){
+totaalKnop.addEventListener("click", function (event) {
   resultaat.innerHTML = newKassa.totaal();
 });
 
-
 //controleren of er een nummer zit in de string
 function checkForNumber(t) {
-    let regex = /\d/g;
-    return regex.test(t);
+  let regex = /\d/g;
+  return regex.test(t);
 }
