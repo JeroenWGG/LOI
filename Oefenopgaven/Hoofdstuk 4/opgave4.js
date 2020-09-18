1db423e585a3df4cc967172d28a6e73784484bc1
Jeroen Wiersma <J.Wiersma@plein.nl>
2020-09-18 15:51:26 +0200

    Started opgave 4

---
+++ Oefenopgaven/Hoofdstuk 4/opgave4.js
@@ -0,0 +1,39 @@
+let resultDiv = document.querySelector("#result");
+let arrayViewer = [1, 2, 3, 4, 5];
+
+function outputArray(arr) {
+  for (ar of arr) {
+    resultDiv.append(ar);
+  }
+}
+
+let addToArrayButton = document.querySelector("#add");
+addToArrayButton.addEventListener("click", addToArray);
+
+outputArray(arrayViewer);
+
+function clearArray() {
+  resultDiv.innerHTML = "";
+}
+
+function addToArray() {
+  arrayViewer.push(Number(document.querySelector("#inputField").value));
+  clearArray();
+  outputArray(arrayViewer);
+}
+
+let removeFromArrayButton = document.querySelector("#remove");
+removeFromArrayButton.addEventListener("click", removeNumber);
+
+function removeNumber() {
+  arrayViewer.pop();
+  clearArray();
+  outputArray(arrayViewer);
+}
+
+let sortArray = document.querySelector("#sortArray");
+sortArray.addEventListener("click", function () {
+  arrayViewer.sort(function (a, b) {
+    return a - b;
+  });
+});
