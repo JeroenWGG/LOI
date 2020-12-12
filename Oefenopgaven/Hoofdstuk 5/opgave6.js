0fd99d78bcd8938d312c5f73fc07e80167cd6179
Jeroen <jeroen_wiersma1995@live.nl>
2020-12-12 16:00:49 +0100

    Finished opgave 6

--- Oefenopgaven/Hoofdstuk 5/opgave6.js
+++ Oefenopgaven/Hoofdstuk 5/opgave6.js
@@ -1,31 +1,30 @@
 //declare variables
 let getVerstuurKnop = document.querySelector("#verstuurKnop");
 let getForm = document.querySelector("#formContainer");
-let getSelectVoornaam = document.querySelector(".selectVoornaam");
-let getSelectAchternaam = document.querySelector(".selectAchternaam");
+let getVoornaamInput, getAchternaamInput, getPuntenInput;
+let getTable = document.querySelector("#tableContainer");
 let output = document.querySelector(".output");
 
 //declare eventlisteners
-getVerstuurKnop.addEventListener("click", showInformation);
+getVerstuurKnop.addEventListener("click", addInformation);
 
 //functions
-function showInformation() {
-  output.innerHTML = "";
-  let getSelectVoornaamOption =
-    getSelectVoornaam.options[getSelectVoornaam.selectedIndex].value;
-  let p1 = document.createElement("p");
-  let pText1 = document.createTextNode(
-    "Voornaam is " + getSelectVoornaamOption
-  );
-  p1.appendChild(pText1);
-  output.appendChild(p1);
-
-  let getSelectAchternaamOption =
-    getSelectAchternaam.options[getSelectAchternaam.selectedIndex].value;
-  let p2 = document.createElement("p");
-  let pText2 = document.createTextNode(
-    "Achternaam is " + getSelectAchternaamOption
-  );
-  p2.appendChild(pText2);
-  output.appendChild(p2);
+function addInformation() {
+  getVoornaamInput = document.querySelector("#voornaamInput").value;
+  getAchternaamInput = document.querySelector("#achternaamInput").value;
+  getPuntenInput = document.querySelector("#puntenInput").value;
+  let tr = document.createElement("tr");
+  let td1 = document.createElement("td");
+  let td1Text = document.createTextNode(getVoornaamInput);
+  td1.appendChild(td1Text);
+  let td2 = document.createElement("td");
+  let td2Text = document.createTextNode(getAchternaamInput);
+  td2.appendChild(td2Text);
+  let td3 = document.createElement("td");
+  let td3Text = document.createTextNode(getPuntenInput);
+  td3.appendChild(td3Text);
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+  getTable.appendChild(tr);
 }
